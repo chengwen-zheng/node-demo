109c756167acd4359c6ebf1593a29dd297f9cbd8
leon.zheng <leon.zheng@derbysoft.com>
2019-12-20 15:57:10 +0800

    update

--- node-api-demo/cluster-demo/app.js
+++ node-api-demo/cluster-demo/app.js
@@ -8,17 +8,21 @@ let server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html'});
 
     setTimeout(() => {
+
+        // 模拟发生错误
+        // console.log(window);
         const result = fs.readFileSync(`./index.htm`, 'utf-8')
         res.end(result);
 
         // 模拟内存泄漏
-        leak.push(result);
+        // leak.push(result);
     }, 50);
 })
 
 
 server.listen(3000, () => {
     console.log('正在监听3000端口');
-    while (true) {}
+    // 模拟堵塞造成的假死
+    // while (true) {}
 })
 
